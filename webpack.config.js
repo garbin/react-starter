9c8bcfcff5abf077b9b4266809623e4fd1eeb6f1
Garbin Huang <garbinh@gmail.com>
2015-12-25 07:13:01 +0000

    *

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
   },
   output:{
     path: path.join(__dirname, 'dist'),
-    filename:'[name].js',
+    filename:'js/[name].js',
     publicPath: '/assets/',
   },
   resolve:{
